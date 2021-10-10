b308e8e9348aeabbd2a366498f6fcde291a2fda5
federico chaves <federico.chaves@gmail.com>
2021-10-09 22:07:59 -0500

    cambios tarifas

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ from flask import render_template, request, redirect
 from config_lang import ConfigLang
 
 
+
 app = Flask(__name__)
 
 #'postgresql://username:password@host:port/database'
 
 uri = 'postgresql://zgnojbpnkhoqmx:5d8d7241e51758b68ce3aa6c365d746d4ea3b8a711a2b5d31c33100ef7a6705a@ec2-44-196-146-152.compute-1.amazonaws.com:5432/d26fib3rqoq9p1' # produccion
 
-from config import config
-uri = config() # megavas
+#refrescafrom config import config
+#uri = config() # megavas
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,10 +24,10 @@ bcrypt = Bcrypt(app)
 
 from models.rol import Rol
 from models.documento import Documento
+from models.tipovehiculo import TipoVehiculo
 from models.tarifa import Tarifa
 from models.usuario import Usuario
 from models.parqueadero import Parqueadero
-from models.tipovehiculo import TipoVehiculo
 from models.vehiculo import Vehiculo
 from models.factura import Factura
 
@@ -62,7 +63,29 @@ def login():
     
     return str(Usuario.login(usuario, password))
 
-@app.route("/users")
-def users():
+@app.route("/tarifas", methods=["GET","POST"])
+def tarifas():
+    tarifa=Tarifa.get_full()
+    bici=Tarifa.trae_tarifa(1)  
+    moto=Tarifa.trae_tarifa(2) 
+    auto=Tarifa.trae_tarifa(3)  
+    if request.method == 'POST':
+       valor1=request.form["bici"]
+       valor2=request.form["moto"]
+       valor3=request.form["auto"]
+       tar=Tarifa(1,valor1)
+       print(tar)
+       tar.actualiza_tarifa(1,tar)
+       tar=Tarifa(2,valor2)
+       tar.actualiza_tarifa(2,tar)
+       tar=Tarifa(3,valor3)
+       tar.actualiza_tarifa(3,tar)
+    return render_template('tarifas.html',tarifa=tarifa,bici=bici.valor,moto=moto.valor,auto=auto.valor)
+
     
-    return render_template('pruebas.html')
+
+
+    
+    
+       
+    
--- models/tarifa.py
+++ models/tarifa.py
@@ -1,4 +1,5 @@
 from app import database
+from app import TipoVehiculo
 
 class Tarifa(database.Model):
     __tablename__ = 'tarifas'
@@ -10,10 +11,30 @@ class Tarifa(database.Model):
     def create(self):
         database.session.add(self)
         database.session.commit()
-
+        
+    def __init__(self,id_tv,valor):
+        self.id_tv = id_tv
+        self.valor = valor
+        
     def __str__(self):
         return f"<Tarifa: {self.id_t} {self.id_tv} {self.valor}>"
     
     @staticmethod
     def get_all():
         return Tarifa.query.all()
+    
+    @staticmethod
+    def get_full():
+        return database.session.query(Tarifa, TipoVehiculo).join(TipoVehiculo).all()
+    
+    @staticmethod
+    def trae_tarifa(id):
+        return  Tarifa.query.filter_by(id_tv=id).first()
+        
+    @staticmethod
+    def actualiza_tarifa(id,tarifa):
+        pre_tarifa=Tarifa.trae_tarifa(id)
+        pre_tarifa.valor=tarifa.valor
+        database.session.commit()
+        
+    
